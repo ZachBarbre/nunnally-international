66ff1857d98bf41873ed0253ab3a268d40cd2f56
Zach Barbre <zach.barbre@gmail.com>
2021-02-10 18:43:58 -0500

    add new report

--- src/routes/insights.svelte
+++ src/routes/insights.svelte
@@ -10,14 +10,22 @@
 	const reportsInfoText = 'Market Intelligence: Access our latest market analysis and research on the RIA M&A market';
 	const reports = {reports: [
 		{
+			superTitle: 'RIA M&A Market Report',
 			title: 'How Much is My RIA Worth?',
 			description: 'Evaluation of the impact of Covid-19 and subsequent market volatility on valuations.',
 			downloadURI: '/RIA M&A Market Report - How Much is My RIA Worth.pdf'
 		},
 		{
+			superTitle: 'RIA M&A Market Report',
 			title: 'Is It the Right Time to Sell?',
 			description: 'Analysis of the 3 primary factors that are driving RIA M&A deals to record highs.',
 			downloadURI: '/RIA M&A MARKET REPORT - Is it the Right Time to Sell.pdf'
+		},
+		{
+			superTitle: 'RIA M&A Advice Series (Part I of III)',
+			title: 'Is It the Right Time to Sell?',
+			description: 'Analysis of the 3 primary factors that are driving RIA M&A deals to record highs.',
+			downloadURI: '/RIA M&A Advice Series - Strategic Objectives (Part I of III).pdf'
 		},
 	]};
 

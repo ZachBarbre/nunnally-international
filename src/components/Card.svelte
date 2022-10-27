9299cbb1cec7917a531432f100932e1f4e33a40c
Zach Barbre <zach.barbre@gmail.com>
2022-10-26 20:55:07 -0400

    upate image, fix css zoom bug

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -10,20 +10,19 @@ section {
     padding: 4rem 0;
     background-size: cover;
 }
-.card {
-    
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-between;
-    align-items: center;
+/* .card {   
+} */
+
+.card-top {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+    justify-items: center;
+    gap: 2rem;
 }
 
 .img-wrapper {
-    /* height: 35vh; */
     flex: 0 1 500px;
     min-width: 200px;
-    position: relative;
-    /* padding-left: 5px; */
 }
 
 img {
@@ -37,7 +36,7 @@ img {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    /* padding: 0 1.5rem; */
+    padding: 0 1.5rem;
 }
 
 .content p {
@@ -64,7 +63,10 @@ img {
         flex: 0 1 auto;
     }
 }
-@media (max-width: 430px) {
+@media (max-width: 450px) {
+    .card-top {
+        grid-template-columns: unset;
+    }
     .content {
         padding: 0;
     }
@@ -74,29 +76,32 @@ img {
 <section style="{background}">
     <div class="wrapper">
         <div class="card">
-            {#if isFipped}            
-                <div class="content" style="{background.includes('url') ? 'background: #FFF' : ''} {sideStyle}">
-                    <h3>{header}</h3>   
-                    <p>{text}</p>
-                    <slot name="side"></slot>
-                </div>
-                <div class="img-wrapper">
-                    {#if imgUrl}
-                        <img src="{imgUrl}" alt="{imgAlt}">
-                    {/if}
-                </div>
-            {:else}
-                <div class="img-wrapper">
-                    {#if imgUrl}
-                        <img src="{imgUrl}" alt="{imgAlt}">
-                    {/if}
-                </div>
-                <div class="content" style="{background.includes('url') ? 'background: #FFF' : ''} {sideStyle}">
-                    <h3>{header}</h3>
-                    <p>{text}</p>
-                    <slot name="side"></slot>
-                </div>
-            {/if}
+            <div class="card-top">
+
+                {#if isFipped}            
+                    <div class="content" style="{background.includes('url') ? 'background: #FFF' : ''} {sideStyle}">
+                        <h3>{header}</h3>   
+                        <p>{text}</p>
+                        <slot name="side"></slot>
+                    </div>
+                    <div class="img-wrapper">
+                        {#if imgUrl}
+                            <img src="{imgUrl}" alt="{imgAlt}">
+                        {/if}
+                    </div>
+                {:else}
+                    <div class="img-wrapper">
+                        {#if imgUrl}
+                            <img src="{imgUrl}" alt="{imgAlt}">
+                        {/if}
+                    </div>
+                    <div class="content" style="{background.includes('url') ? 'background: #FFF' : ''} {sideStyle}">
+                        <h3>{header}</h3>
+                        <p>{text}</p>
+                        <slot name="side"></slot>
+                    </div>
+                {/if}
+            </div>
             <slot name="under"></slot>
         </div>
     </div>
---
+++ src/components/Testimonials.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let content;
+
+  const testimonialArray = content.slice(0,2)
+</script>
+
+<style>
+ section {
+  background-color: #A57F60;
+ }
+
+ 
+</style>
+
+<section>
+  <h3></h3>
+  {#each testimonialArray as testimonial}
+    <div>
+      
+    </div>
+  {/each}
+</section>

8a8077841e9ad13cd27c3d52fd298288df0c0d06
Zach Barbre <zachbarbre@proton.me>
2025-04-03 20:58:19 -0400

    added new two new tesitmonials, added new deal

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,104 +1,116 @@
 <script>
-  import Card from "../components/Card.svelte";
-  import Hero from "../components/Hero.svelte";
-  import Info from "../components/Info.svelte";
-  import BulletPoints from "../components/BulletPoints.svelte";
-  import Testimonials from "../components/Testimonials.svelte";
+    import Card from "../components/Card.svelte";
+    import Hero from "../components/Hero.svelte";
+    import Info from "../components/Info.svelte";
+    import BulletPoints from "../components/BulletPoints.svelte";
+    import Testimonials from "../components/Testimonials.svelte";
 
-  const heroText = "Perspective. Transparency. Perseverance.";
-  const background = `background: url(/thumbs-up.jpg) top 46% center no-repeat;`;
-  const infoText =
-    "Nunnally International provides strategic M&A services to RIA firms that are looking to buy, sell or merge their business.";
+    const heroText = "Perspective. Transparency. Perseverance.";
+    const background = `background: url(/thumbs-up.jpg) top 46% center no-repeat;`;
+    const infoText =
+        "Nunnally International provides strategic M&A services to RIA firms that are looking to buy, sell or merge their business.";
 
-  const firstCardContent = {
-    imgUrl: "/financial-advice.jpg",
-    imgAlt: "A man and woman receiving financial advice.",
-    text: "We've spent years in the investment management industry in multiple capacities and understand the risks, opportunities and challenges that advisors face on a daily basis. We understand that every RIA is uniquely different and we tailor our  M&A guidance to your specific goals and circumstances.",
-    background: "background: #D6DDD9;",
-    header: "MARKET KNOWLEDGE",
-    isFipped: true,
-  };
+    const firstCardContent = {
+        imgUrl: "/financial-advice.jpg",
+        imgAlt: "A man and woman receiving financial advice.",
+        text: "We've spent years in the investment management industry in multiple capacities and understand the risks, opportunities and challenges that advisors face on a daily basis. We understand that every RIA is uniquely different and we tailor our  M&A guidance to your specific goals and circumstances.",
+        background: "background: #D6DDD9;",
+        header: "MARKET KNOWLEDGE",
+        isFipped: true,
+    };
 
-  const secondCardContent = {
-    imgUrl: "/buisness-handshake.jpg",
-    imgAlt: "Two men shaking hands with a third standing behind.",
-    text: "We act in your best interest by adhering to the same fiduciary principles and standard of care that you do. Our incentives are always aligned with yours and we're only compensated if your M&A deal successfully closes.",
-    header: "UNCONFLICTED ADVICE",
-    background: "background: #fefcfb;",
-    // isFipped: true
-  };
+    const secondCardContent = {
+        imgUrl: "/buisness-handshake.jpg",
+        imgAlt: "Two men shaking hands with a third standing behind.",
+        text: "We act in your best interest by adhering to the same fiduciary principles and standard of care that you do. Our incentives are always aligned with yours and we're only compensated if your M&A deal successfully closes.",
+        header: "UNCONFLICTED ADVICE",
+        background: "background: #fefcfb;",
+        // isFipped: true
+    };
 
-  const bulletPointContent = {
-    title: "How We're Different",
-    points: [
-      {
-        img: "/bullet-1-target.svg",
-        imgAlt: "An arrow hitting a bullseye",
-        header: "Strategic Objectives",
-        text: [
-          "What are your goals and how can M&A help you achieve them? Is your intent to grow, diversify, adapt or create a succession plan?",
+    const bulletPointContent = {
+        title: "How We're Different",
+        points: [
+            {
+                img: "/bullet-1-target.svg",
+                imgAlt: "An arrow hitting a bullseye",
+                header: "Strategic Objectives",
+                text: [
+                    "What are your goals and how can M&A help you achieve them? Is your intent to grow, diversify, adapt or create a succession plan?",
+                ],
+            },
+            {
+                img: "/bullet-2-handshake.svg",
+                imgAlt: "Handshake",
+                header: "Partnership Criteria",
+                text: [
+                    "Who can help you achieve your objectives? What characteristics define an ideal strategic partner? What's that's profile look like?",
+                ],
+            },
+            {
+                img: "/bullet-3-puzzle.svg",
+                imgAlt: "Interlocking puzzle pieces",
+                header: "Firm Culture",
+                text: [
+                    "Do you share the same worldview, investment philosophy and ambitions? What synergies exists professionally and personally between firm principles?",
+                ],
+            },
         ],
-      },
-      {
-        img: "/bullet-2-handshake.svg",
-        imgAlt: "Handshake",
-        header: "Partnership Criteria",
-        text: [
-          "Who can help you achieve your objectives? What characteristics define an ideal strategic partner? What's that's profile look like?",
-        ],
-      },
-      {
-        img: "/bullet-3-puzzle.svg",
-        imgAlt: "Interlocking puzzle pieces",
-        header: "Firm Culture",
-        text: [
-          "Do you share the same worldview, investment philosophy and ambitions? What synergies exists professionally and personally between firm principles?",
-        ],
-      },
-    ],
-    background: "background: #D6DDD9;",
-    width: "",
-  };
+        background: "background: #D6DDD9;",
+        width: "",
+    };
 
-  const testimonialContent = {
-    title: "What our clients say",
-    testimonials: [
-      {
-        text: `We could not be happier with Nunnally International's representation of Loveless Wealth Management in our M&A transaction. Their attention to detail and organization were second to none. Tyler introduced us to just the right firm and negotiated a great deal on our behalf.`,
-        name: "Don Loveless",
-        title1: "Founder and CEO",
-        title2: "Loveless Wealth Management",
-      },
-      {
-        text: `After searching and speaking with a number of firms for a couple years I was about to hit pause. Tyler came along and was able to very accurately identify the type of firm that would fit my needs. He knew the landscape, the deals, and how to narrow down possible partners. It was refreshing to have someone of his experience filter through and eventually identify my partner firm.`,
-        name: "Lou Della Valle",
-        title1: "Principal",
-        title2: "C2C Wealth Management",
-      },
-      {
-        text: `Tyler's personal interaction with each of our team members helped solidify what we were all looking for with a potential merger.  His ability to formalize the process of meeting with potential suitors was extremely valuable.  Most importantly, he helped our team stay on the same page as a cohesive unit with a tangible goal in sight.`,
-        name: "Brian Suskevitch",
-        title1: "Director, Senior Wealth Advisor",
-        title2: "Partner and Shareholder",
-      },
-      {
-        text: `I could not imagine navigating the M&A process on our own without Nunnally International. There is an overwhelming amount of detail and care needed throughout the entire process that was foreign to us, and Tyler was able to offer insight and guidance in our quest to find the best possible partner. He took the time to get to know us and we could not be happier with the final result.`,
-        name: "Betsy Rector",
-        title1: "Director, Senior Wealth Advisor",
-        title2: "Partner and Shareholder",
-      },
-      {
-        text: `Always a pleasure working with Tyler and Nunnally International.  Thank you!  Appreciate your ability to identify high caliber firms, clearly articulate needs and priorities for both parties, and helpfully contribute to making the transaction happen.`,
-        name: "Bryan Keller",
-        title1: "Chief Strategic Officer",
-        title2: "Dakota Wealth Management",
-      },
-    ],
-  };
+    const testimonialContent = {
+        title: "What our clients say",
+        testimonials: [
+            {
+                text: `It has been a real pleasure working with Nunnally International. Tyler Nunnally is a true professional. He moved the M&A process forward at a good pace, and he helped me identify the perfect fit for my practice and my clients. I would highly recommend Nunnally International to any RIA who is interested in M&A.  `,
+                name: "Scott Collins",
+                title1: "President",
+                title2: "Fiduciary Wealth Advisors",
+            },
+            {
+                text: `It was a pleasure working with Tyler and Nunnally International. Tyler's ability to identify high caliber firms that fit our culture and goals is a rare and prized skill. We look forward to a continuously fruitful relationship with Nunnally international.`,
+                name: "Paul Kleinschmidt",
+                title1: "President & CEO ",
+                title2: "Tocqueville Asset Management",
+            },
+            {
+                text: `We could not be happier with Nunnally International's representation of Loveless Wealth Management in our M&A transaction. Their attention to detail and organization were second to none. Tyler introduced us to just the right firm and negotiated a great deal on our behalf.`,
+                name: "Don Loveless",
+                title1: "Founder and CEO",
+                title2: "Loveless Wealth Management",
+            },
+            {
+                text: `After searching and speaking with a number of firms for a couple years I was about to hit pause. Tyler came along and was able to very accurately identify the type of firm that would fit my needs. He knew the landscape, the deals, and how to narrow down possible partners. It was refreshing to have someone of his experience filter through and eventually identify my partner firm.`,
+                name: "Lou Della Valle",
+                title1: "Principal",
+                title2: "C2C Wealth Management",
+            },
+            {
+                text: `Tyler's personal interaction with each of our team members helped solidify what we were all looking for with a potential merger.  His ability to formalize the process of meeting with potential suitors was extremely valuable.  Most importantly, he helped our team stay on the same page as a cohesive unit with a tangible goal in sight.`,
+                name: "Brian Suskevitch",
+                title1: "Director, Senior Wealth Advisor",
+                title2: "Partner and Shareholder",
+            },
+            {
+                text: `I could not imagine navigating the M&A process on our own without Nunnally International. There is an overwhelming amount of detail and care needed throughout the entire process that was foreign to us, and Tyler was able to offer insight and guidance in our quest to find the best possible partner. He took the time to get to know us and we could not be happier with the final result.`,
+                name: "Betsy Rector",
+                title1: "Director, Senior Wealth Advisor",
+                title2: "Partner and Shareholder",
+            },
+            {
+                text: `Always a pleasure working with Tyler and Nunnally International.  Thank you!  Appreciate your ability to identify high caliber firms, clearly articulate needs and priorities for both parties, and helpfully contribute to making the transaction happen.`,
+                name: "Bryan Keller",
+                title1: "Chief Strategic Officer",
+                title2: "Dakota Wealth Management",
+            },
+        ],
+    };
 </script>
 
 <svelte:head>
-  <title>Nunnally International</title>
+    <title>Nunnally International</title>
 </svelte:head>
 
 <Hero {heroText} {background} />
@@ -110,22 +122,22 @@
 <Card content={secondCardContent} />
 
 <BulletPoints content={bulletPointContent}>
-  <p>
-    Nunnally International employs investor profiling expertise to help you find
-    the best fit. We get to know you and your firm personally in order to gain a
-    clear understanding of the factors that are key to your strategic
-    partnership success.
-  </p>
+    <p>
+        Nunnally International employs investor profiling expertise to help you
+        find the best fit. We get to know you and your firm personally in order
+        to gain a clear understanding of the factors that are key to your
+        strategic partnership success.
+    </p>
 </BulletPoints>
 
 <Testimonials content={testimonialContent} />
 
 <style>
-  p {
-    width: 75%;
-    text-align: center;
-    font-size: 1.2rem;
-    max-width: 1200px;
-    margin-bottom: 5%;
-  }
+    p {
+        width: 75%;
+        text-align: center;
+        font-size: 1.2rem;
+        max-width: 1200px;
+        margin-bottom: 5%;
+    }
 </style>
--- src/routes/insights.svelte
+++ src/routes/insights.svelte
@@ -1,153 +1,166 @@
 <script>
-  import Hero from "../components/Hero.svelte";
-  import Info from "../components/Info.svelte";
-  import Reports from "../components/Reports.svelte";
-  import Deals from "../components/Deals.svelte";
+    import Hero from "../components/Hero.svelte";
+    import Info from "../components/Info.svelte";
+    import Reports from "../components/Reports.svelte";
+    import Deals from "../components/Deals.svelte";
 
-  const heroText = "Perspective. Transparency. Perseverance.";
-  const background = `background: url(/high-five.jpg) top 25% center no-repeat;`;
+    const heroText = "Perspective. Transparency. Perseverance.";
+    const background = `background: url(/high-five.jpg) top 25% center no-repeat;`;
 
-  const reportsInfoText =
-    "Market Intelligence: Access our latest market analysis and research on the RIA M&A market";
-  const reports = {
-    reports: [
-      {
-        superTitle: "In The News",
-        title: "Press Release - ",
-        boldDescription:
-          "Nunnally International serves as M&A Advisor in Poplar Forest Capital merger with Tocqueville Asset Management",
-        description: "",
-        downloadURI:
-          "/Tocqueville Asset Management Welcomes Poplar Forest Capital Team.pdf",
-      },
-      {
-        superTitle: "RIA M&A Advice Series (Part I of II)",
-        title: "BEHAVIORAL FINANCE IN M&A",
-        description: "Beware of Biases That Cost Millions",
-        downloadURI:
-          "/RIA M&A Advice Series - Beware of Biases That Cost Millions.pdf",
-      },
-      {
-        superTitle: "RIA M&A Advice Series",
-        title: "HOW MUCH IS YOUR RIA WORTH?",
-        description: "HOW MUCH IS YOUR RIA WORTH?",
-        downloadURI: "/RIA M&A Advice Series - How Much Is Your RIA Worth.pdf",
-      },
-      {
-        superTitle: "In The News",
-        title: "PR Newswire - ",
-        boldDescription:
-          "Nunnally International serves as M&A Advisor in acquisition of C2C Wealth Management",
-        description: "",
-        downloadURI: "/Dakota-Wealth-Management-Acquires-C2C-Wealth.pdf",
-      },
-      {
-        superTitle: "RIA M&A Advice Series (Part II of II)",
-        title: "EXTERNAL SUCCESSION",
-        description: "The Stark Reality of Succession Planning",
-        downloadURI:
-          "/RIA M&A Advice Series - External Succession (Part II of II).pdf",
-      },
-      {
-        superTitle: "RIA M&A Advice Series (Part I of II)",
-        title: "INTERNAL SUCCESSION",
-        description: "The Stark Reality of Succession Planning",
-        downloadURI:
-          "/RIA M&A Advice Series - Internal Succession (Part I of II).pdf",
-      },
-      {
-        superTitle: "RIA M&A Advice Series (Part III of III)",
-        title: "Firm culture",
-        description:
-          "3 factors that are key to your RIA M&A partnership success.",
-        downloadURI: "/RIA M&A Advice Series - Firm Culture (Part 3 of 3).pdf",
-      },
-      {
-        superTitle: "In The News",
-        title: "CITYWIRE RIA -",
-        boldDescription:
-          "Nunnally International represented Loveless Wealth Management in sale to Dakota Wealth Management",
-        description: "",
-        downloadURI:
-          "/CityWire-Nunnally-International-represented-Loveless-Wealth-Management-in-sale-to-Dakota-Wealth-Management.pdf",
-      },
-      {
-        superTitle: "RIA M&A Advice Series (Part II of III)",
-        title: "PARTNERSHIP CRITERIA",
-        description:
-          "3 factors that are key to your RIA M&A partnership success.",
-        downloadURI:
-          "/RIA M&A Advice Series - Partnership Criteria (Part II of III).pdf",
-      },
-      {
-        superTitle: "RIA M&A Advice Series (Part I of III)",
-        title: "STRATEGIC OBJECTIVES",
-        description:
-          "3 factors that are key to your RIA M&A partnership success.",
-        downloadURI:
-          "/RIA M&A Advice Series - Strategic Objectives (Part I of III).pdf",
-      },
-      {
-        superTitle: "RIA M&A Market Report",
-        title: "How Much is My RIA Worth?",
-        boldDescription: "",
-        description:
-          "Evaluation of the impact of Covid-19 and subsequent market volatility on valuations.",
-        downloadURI: "/RIA M&A Market Report - How Much is My RIA Worth.pdf",
-      },
-      {
-        superTitle: "RIA M&A Market Report",
-        title: "Is It the Right Time to Sell?",
-        description:
-          "Analysis of the 3 primary factors that are driving RIA M&A deals to record highs.",
-        downloadURI:
-          "/RIA M&A MARKET REPORT - Is it the Right Time to Sell.pdf",
-      },
-    ],
-  };
+    const reportsInfoText =
+        "Market Intelligence: Access our latest market analysis and research on the RIA M&A market";
+    const reports = {
+        reports: [
+            {
+                superTitle: "In The News",
+                title: "Press Release - ",
+                boldDescription:
+                    "Nunnally International serves as M&A Advisor in Poplar Forest Capital merger with Tocqueville Asset Management",
+                description: "",
+                downloadURI:
+                    "/Tocqueville Asset Management Welcomes Poplar Forest Capital Team.pdf",
+            },
+            {
+                superTitle: "RIA M&A Advice Series (Part I of II)",
+                title: "BEHAVIORAL FINANCE IN M&A",
+                description: "Beware of Biases That Cost Millions",
+                downloadURI:
+                    "/RIA M&A Advice Series - Beware of Biases That Cost Millions.pdf",
+            },
+            {
+                superTitle: "RIA M&A Advice Series",
+                title: "HOW MUCH IS YOUR RIA WORTH?",
+                description: "HOW MUCH IS YOUR RIA WORTH?",
+                downloadURI:
+                    "/RIA M&A Advice Series - How Much Is Your RIA Worth.pdf",
+            },
+            {
+                superTitle: "In The News",
+                title: "PR Newswire - ",
+                boldDescription:
+                    "Nunnally International serves as M&A Advisor in acquisition of C2C Wealth Management",
+                description: "",
+                downloadURI:
+                    "/Dakota-Wealth-Management-Acquires-C2C-Wealth.pdf",
+            },
+            {
+                superTitle: "RIA M&A Advice Series (Part II of II)",
+                title: "EXTERNAL SUCCESSION",
+                description: "The Stark Reality of Succession Planning",
+                downloadURI:
+                    "/RIA M&A Advice Series - External Succession (Part II of II).pdf",
+            },
+            {
+                superTitle: "RIA M&A Advice Series (Part I of II)",
+                title: "INTERNAL SUCCESSION",
+                description: "The Stark Reality of Succession Planning",
+                downloadURI:
+                    "/RIA M&A Advice Series - Internal Succession (Part I of II).pdf",
+            },
+            {
+                superTitle: "RIA M&A Advice Series (Part III of III)",
+                title: "Firm culture",
+                description:
+                    "3 factors that are key to your RIA M&A partnership success.",
+                downloadURI:
+                    "/RIA M&A Advice Series - Firm Culture (Part 3 of 3).pdf",
+            },
+            {
+                superTitle: "In The News",
+                title: "CITYWIRE RIA -",
+                boldDescription:
+                    "Nunnally International represented Loveless Wealth Management in sale to Dakota Wealth Management",
+                description: "",
+                downloadURI:
+                    "/CityWire-Nunnally-International-represented-Loveless-Wealth-Management-in-sale-to-Dakota-Wealth-Management.pdf",
+            },
+            {
+                superTitle: "RIA M&A Advice Series (Part II of III)",
+                title: "PARTNERSHIP CRITERIA",
+                description:
+                    "3 factors that are key to your RIA M&A partnership success.",
+                downloadURI:
+                    "/RIA M&A Advice Series - Partnership Criteria (Part II of III).pdf",
+            },
+            {
+                superTitle: "RIA M&A Advice Series (Part I of III)",
+                title: "STRATEGIC OBJECTIVES",
+                description:
+                    "3 factors that are key to your RIA M&A partnership success.",
+                downloadURI:
+                    "/RIA M&A Advice Series - Strategic Objectives (Part I of III).pdf",
+            },
+            {
+                superTitle: "RIA M&A Market Report",
+                title: "How Much is My RIA Worth?",
+                boldDescription: "",
+                description:
+                    "Evaluation of the impact of Covid-19 and subsequent market volatility on valuations.",
+                downloadURI:
+                    "/RIA M&A Market Report - How Much is My RIA Worth.pdf",
+            },
+            {
+                superTitle: "RIA M&A Market Report",
+                title: "Is It the Right Time to Sell?",
+                description:
+                    "Analysis of the 3 primary factors that are driving RIA M&A deals to record highs.",
+                downloadURI:
+                    "/RIA M&A MARKET REPORT - Is it the Right Time to Sell.pdf",
+            },
+        ],
+    };
 
-  const dealsInfoText = "M&A Deals: Our most recently completed transactions";
-  const deals = {
-    deals: [
-      {
-        acquiredLogo: "/poplar-logo.png",
-        acquiredAlt: "Poplar Forest Capital Logo",
-        acquiredByLogo: "/tocqueville-logo.png",
-        acquiredByAlt: "Tocqueville Logo",
-        acquiredByTextOveride: "has merged with",
-        acquiredText:
-          "Nunnally International served as M&A Advisor in the transaction",
-      },
-      {
-        acquiredLogo: "/c2d-logo.png",
-        acquiredAlt: "C2C Wealth Management LLC Logo",
-        acquiredByLogo: "/dakota-logo.png",
-        acquiredByAlt: "Dakota Wealth Management Logo",
-        acquiredText:
-          "Nunnally International served as M&A Advisor in the transaction",
-      },
-      {
-        acquiredLogo: "/loveless-logo.png",
-        acquiredAlt: "Loveless Wealth Management LLC Logo",
-        acquiredByLogo: "/dakota-logo.png",
-        acquiredByAlt: "Dakota Wealth Management Logo",
-        acquiredText:
-          "Nunnally International served as M&A Advisor in the transaction",
-      },
-      {
-        acquiredLogo: "/pfa-logo.png",
-        acquiredAlt: "Personal Financial Advisors Logo",
-        acquiredByLogo: "/mercer-logo.png",
-        acquiredByAlt: "Mercer Advisors Logo",
-        acquiredText:
-          "Nunnally International served as M&A Advisor in the transaction",
-      },
-    ],
-  };
+    const dealsInfoText = "M&A Deals: Our most recently completed transactions";
+    const deals = {
+        deals: [
+            {
+                acquiredLogo: "/fiduciary-logo.png",
+                acquiredAlt: "Fiduciary Wealth Advisors Logo",
+                acquiredByLogo: "/waverly-logo.png",
+                acquiredByAlt: "Waverly Advisors Logo",
+                acquiredByTextOveride: "has been acquired by",
+                acquiredText:
+                    "Nunnally International served as M&A Advisor in the transaction",
+            },
+            {
+                acquiredLogo: "/poplar-logo.png",
+                acquiredAlt: "Poplar Forest Capital Logo",
+                acquiredByLogo: "/tocqueville-logo.png",
+                acquiredByAlt: "Tocqueville Logo",
+                acquiredByTextOveride: "has merged with",
+                acquiredText:
+                    "Nunnally International served as M&A Advisor in the transaction",
+            },
+            {
+                acquiredLogo: "/c2d-logo.png",
+                acquiredAlt: "C2C Wealth Management LLC Logo",
+                acquiredByLogo: "/dakota-logo.png",
+                acquiredByAlt: "Dakota Wealth Management Logo",
+                acquiredText:
+                    "Nunnally International served as M&A Advisor in the transaction",
+            },
+            {
+                acquiredLogo: "/loveless-logo.png",
+                acquiredAlt: "Loveless Wealth Management LLC Logo",
+                acquiredByLogo: "/dakota-logo.png",
+                acquiredByAlt: "Dakota Wealth Management Logo",
+                acquiredText:
+                    "Nunnally International served as M&A Advisor in the transaction",
+            },
+            {
+                acquiredLogo: "/pfa-logo.png",
+                acquiredAlt: "Personal Financial Advisors Logo",
+                acquiredByLogo: "/mercer-logo.png",
+                acquiredByAlt: "Mercer Advisors Logo",
+                acquiredText:
+                    "Nunnally International served as M&A Advisor in the transaction",
+            },
+        ],
+    };
 </script>
 
 <svelte:head>
-  <title>Our Services | Nunnally International</title>
+    <title>Our Services | Nunnally International</title>
 </svelte:head>
 
 <Hero {heroText} {background} />
